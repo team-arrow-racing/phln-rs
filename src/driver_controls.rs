ecbb284d7c6fc6b67718ddd371a634a43014d3df
Liam Kinne <liam@kinne.io>
2023-03-01 11:48:27 +1000

    add motor controller commands

--- src/driver_controls.rs
+++ src/driver_controls.rs
@@ -27,6 +27,38 @@ impl DriverControls {
         Self { base_id }
     }
 
+    /// Motor drive command
+    pub fn motor_drive(self, velocity_rpm: f32, current_percent: f32) -> Frame {
+        let id = StandardId::new(self.base_id + 0x01).unwrap();
+
+        let vel = velocity_rpm.to_le_bytes();
+        let cur = current_percent.to_be_bytes();
+
+        let data = [
+            vel[0], vel[1], vel[2], vel[3], cur[0], cur[1], cur[2], cur[3],
+        ];
+
+        Frame::new_data(id, data)
+    }
+
+    /// Motor power command
+    pub fn motor_power(self, bus_current_percent: f32) -> Frame {
+        let id = StandardId::new(self.base_id + 0x02).unwrap();
+
+        let bus = bus_current_percent.to_le_bytes();
+
+        let data = [0, 0, 0, 0, bus[0], bus[1], bus[2], bus[3]];
+
+        Frame::new_data(id, data)
+    }
+
+    /// Reset WaveSculptor
+    pub fn reset_wavesculptor(self) -> Frame {
+        let id = StandardId::new(self.base_id + 0x03).unwrap();
+
+        Frame::new_data(id, [0; 8])
+    }
+
     /// Form a switch position frame
     pub fn switch_position(self, ignition_position: IgnitionPosition) -> Frame {
         let id = StandardId::new(self.base_id + 0x05).unwrap();
